b46d1fe98561662a3d66ed6fd33006b0a3accaa1
seiyu-nico <yuseiishii141@gmail.com>
2023-04-25 22:22:41 +0900

    メモ削除

--- src/app.py
+++ src/app.py
@@ -11,10 +11,3 @@ app.register_blueprint(foo_module)
 print(f'__name__: {__name__}')
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
-
-
-下記のコマンドでflaskを起動すると`__name_`_が`app`になってしまい`if __name__ == "__main__":`の条件に入りません。
-```
-export FLASK_APP=app.py
-flask run
-````
